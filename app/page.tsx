8c5e969d547bb173b995664e7bf04509072ce07e
poudelsanchit <snchtpdl12@gmail.com>
2025-04-12 15:35:00 +0545

    update 5 files

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
-import { useSession } from "next-auth/react";
 import LandingMain from "./landing/main";
 import NavBar from "./landing/navbar";
 
 export default function Home() {
-  const { data: session } = useSession();
 
   return (
     <>
-      {session && <pre>{JSON.stringify(session.user, null, 10)}</pre>}
       <NavBar />
       <LandingMain />
     </>
